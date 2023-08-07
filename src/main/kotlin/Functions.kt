fb0566b255ee626ad22d38d2f5f81b6119328262
ALLAN BYARUHANGA <byaruhangaallan81@gmail.com>
2023-08-07 12:28:04 +0300

    This my assignment work I had a problem calling functions from main, so I created three different files.

---
+++ src/main/kotlin/Functions.kt
@@ -0,0 +1,32 @@
+fun main(){
+
+//    val a =  1
+//    val b = 3
+//    val z = a+b
+//
+//    print("The sum  of $a and $b is $z")
+    addTwoNumbers()
+   mathCalc()
+
+}
+fun addTwoNumbers(){
+
+    val j = 5
+    val k = 16
+    val c = j+k
+
+    println("Sum of two numbers is $c ")
+
+}
+fun mathCalc(){
+
+    val a = 1
+    val c = 3
+    val k = 9
+
+    val r = a+c
+    val u = a-c
+    val w = a/c
+    val q = a*c
+    println("The values $r, $u,$w,$q")
+}
